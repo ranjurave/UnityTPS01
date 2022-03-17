94ce887c53011518d9f66c7b8414e0b19f861ed9
Ranju <ranjurave@gmail.com>
2022-03-18 12:24:08 +1300

    Third Person Movement Completed
    TODO Jump

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -8,29 +8,40 @@ public class ThirdPersonCamera : MonoBehaviour
     Transform PlayerTransform;
     Transform CameraTransform;
 
-    float MouseMin = 14.0f;
+    float MouseMin = 5.0f; 
     float MouseMax = 50.0f;
     Vector3 CameraOffset;
     Vector3 CameraLookat;
+    float CameraLookHeight = 1.5f;
     float mouseX;
     float mouseY;
     void Start()
     {
+        // getting the transform value of camera.
         CameraTransform = transform;
+        //Set the camera from a distance to player
         CameraOffset = new Vector3(0, 1.75f, -3.35f);
-        CameraLookat = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y + 1.5f, PlayerTransform.position.z);
     }
 
     void Update()
     {
+        //Get mouse input 
         mouseX += Input.GetAxis("Mouse X");
         mouseY += Input.GetAxis("Mouse Y");
         mouseY = Mathf.Clamp(mouseY, MouseMin, MouseMax);
+        // limiting how far camera can go up and down
+        mouseY = Mathf.Clamp(mouseY, MouseMin, MouseMax); 
     }
 
     void LateUpdate() {
+        // Applying mouse position to a Quaternion rotation.
         Quaternion camRotation = Quaternion.Euler(mouseY, mouseX, 0);
+        //Positioning camera around the player using Quaternion * operator
+        //https://docs.unity3d.com/ScriptReference/Quaternion-operator_multiply.html
         CameraTransform.position = PlayerTransform.position + camRotation * CameraOffset;
+        //Creating a look at position from player position and adding height.
+        CameraLookat = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y + CameraLookHeight, PlayerTransform.position.z);
+        //Rotating camera to look at a position
         CameraTransform.LookAt(CameraLookat);
         //Debug.Log(camRotation);
     }
--- Assets/ThirdPersonScript.cs
+++ Assets/ThirdPersonScript.cs
@@ -2,23 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonScript : MonoBehaviour
-{
-    CharacterController ThirdPersonController;
+public class ThirdPersonScript : MonoBehaviour {
+    [SerializeField]
+    Transform PlayerCamera;
 
+    CharacterController ThirdPersonController;
+    float CharacterSpeed = 4;
     float HorizontalMove;
     float VerticalMove;
-    void Start()
-    {
+    float TurnSmoothVelocity;
+    float TurnSmoothTime = 0.25f;
+
+    //variables for jump
+    float JumpSpeed;
+    bool isGrounded;
+    float GroundCheckDistance;
+    LayerMask GroundMask;
+    float gravity;
+    float JumpHeight;
+    Vector3 JumpVelocity;
+
+    void Start() {
         ThirdPersonController = GetComponent<CharacterController>();
     }
 
-
-    void Update()
-    {
+    void Update() {
+        //Getting move button inputs
         HorizontalMove = Input.GetAxis("Horizontal");
         VerticalMove = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(HorizontalMove, 0, VerticalMove);
-        ThirdPersonController.Move(direction);
+        //Creating a vector from the inputs
+        Vector3 direction = new Vector3(HorizontalMove, 0, VerticalMove).normalized;
+
+        //Checking if any input values are coming in. (Magnitude is square root of the total of the squares of of the individual vector components)
+        if (direction.magnitude > 0) {
+            //Get angle from x and z in Radians, convert to Degree, add camera rotation on y axis
+            float TargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PlayerCamera.eulerAngles.y;
+
+            //Smooth the angle with the SmoothDampAngle method
+            float SmoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref TurnSmoothVelocity, TurnSmoothTime);
+
+            //Rotate player to the smoothed angle
+            transform.rotation = Quaternion.Euler(0, SmoothAngle, 0);
+
+            //Apply smoothed angle to forward vector and create a direction vector
+            Vector3 MoveDir = Quaternion.Euler(0f, SmoothAngle, 0f) * Vector3.forward;
+
+            //Apply move to the Character Controller
+            ThirdPersonController.Move(MoveDir * Time.deltaTime * CharacterSpeed);
+        }
+        Jump();
+    }
+
+    void Jump() {
+        //TODO next week.
+
+        isGrounded = Physics.CheckSphere(transform.position, GroundCheckDistance, GroundMask);
+
+        if (!isGrounded) {
+            JumpVelocity.y = -10.0f;
+        }
+
+        ThirdPersonController.Move(JumpVelocity * Time.deltaTime);
+
+        //if (Input.GetKeyDown("Jump")) {
+        //    Vector2 JumpVelocityToAdd = new Vector2(0f, JumpSpeed);
+
+        //}
     }
 }
